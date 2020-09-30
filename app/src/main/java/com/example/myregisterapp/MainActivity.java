3b33ff59c0067451161a31f12a7ecb21a2161618
Shalauddin-bd <mksbd.it@gmail.com>
2020-09-30 22:21:48 +0600

    Profile image load from gallery done.

--- app/src/main/java/com/example/myregisterapp/MainActivity.java
+++ app/src/main/java/com/example/myregisterapp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.myregisterapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView ivProfileImage;
+    static final int PickImage=1;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivProfileImage = findViewById(R.id.imageViewUserImage);
+        ivProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery,"Select image"),PickImage );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PickImage && resultCode== RESULT_OK)
+            imageUri = data.getData();
+       try {
+           Bitmap image = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+           ivProfileImage.setImageBitmap(image);
+       }
+       catch (IOException e){
+            e.printStackTrace();
+       }
     }
 }
